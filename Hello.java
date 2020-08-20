d0d5a11d94a4cfa4e5e9bbeb5dca42c7295dae03
Thanakit Tinop <614259034@webmail.npru.ac.th>
2020-08-20 11:26:23 +0700

    Update text SE to SE2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello SE");
-		System.out.println("Hello SE");
-		System.out.println("Hello SE");
+		System.out.println("Hello SE2");
+		System.out.println("Hello SE2");
+		System.out.println("Hello SE2");
 	}
 }
