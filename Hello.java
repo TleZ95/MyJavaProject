a8ad9402f4ee973ab7f92fe8c273174e00385a46
Thanakit Tinop <614259034@webmail.npru.ac.th>
2020-08-20 11:23:10 +0700

    Fix Hello.java Software Enjineering to SE

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("HelloJava");
+		System.out.println("Hello SE");
+		System.out.println("Hello SE");
+		System.out.println("Hello SE");
 	}
 }
